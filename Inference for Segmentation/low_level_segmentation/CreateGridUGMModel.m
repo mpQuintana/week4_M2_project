d83c5f6d75d8a55e40de8d15afa72fc9df34b8eb
Marc Perez <marcperez1993@gmail.com>
2019-11-26 12:29:30 +0100

    final version of Segmentation exercise

--- Inference for Segmentation/low_level_segmentation/CreateGridUGMModel.m
+++ Inference for Segmentation/low_level_segmentation/CreateGridUGMModel.m
@@ -5,11 +5,45 @@ function [edgePot,edgeStruct]=CreateGridUGMModel(NumFils, NumCols, K, lambda)
 % K: number of states
 % lambda: smoothing factor
 
-
-
 tic
 
-edgePot=[];
-edgeStruct=[];
+nRows = NumFils;
+nCols = NumCols;
+nNodes = nRows*nCols;
+nStates = K;
+
+adj = sparse(nNodes,nNodes);
+
+% Add Down Edges
+ind = 1:nNodes;
+exclude = sub2ind([nRows nCols],repmat(nRows,[1 nCols]),1:nCols); % No Down edge for last row
+ind = setdiff(ind,exclude);
+adj(sub2ind([nNodes nNodes],ind,ind+1)) = 1;
+
+% Add Right Edges
+ind = 1:nNodes;
+exclude = sub2ind([nRows nCols],1:nRows,repmat(nCols,[1 nRows])); % No right edge for last column
+ind = setdiff(ind,exclude);
+adj(sub2ind([nNodes nNodes],ind,ind+nRows)) = 1;
+
+% Add Up/Left Edges
+adj = adj+adj';
+edgeStruct = UGM_makeEdgeStruct(adj,nStates);
+
+edgePot = zeros(nStates,nStates,edgeStruct.nEdges);
+for e = 1:edgeStruct.nEdges
+    pot_same = lambda(2);
+    edgePot_e = ones([nStates, nStates]);
+    for i = 1:nStates
+        edgePot_e(i,i) = pot_same;
+    end
+    %display(edgePotMatrix);
+    edgePot(:,:,e) = edgePot_e;
+end
+
+
+
+
+
 
 toc;
